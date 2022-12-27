e73e36e3e058b94b21e2b93bfb8265b931b059a2
Mobin Kazemi <mobinkazemi@MacBook-Pro.local>
2022-12-27 18:48:36 +0330

    postgres database configured

---
+++ src/database/database.provider.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from 'sequelize-typescript';
+import constants from '../constants';
+
+const {database} = constants;
+
+export const databaseProvider = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({ dialect: 'postgres',...database, });
+      sequelize.addModels([]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
